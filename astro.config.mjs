c7dfb85eb534782ccc7e6acd7f99323a2534288c
SlyVenegas <venegasly@gmail.com>
2025-03-09 15:53:10 -0500

    nueva version proyecto

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  // Add your domain name here
+  site: "https://brinf.vercel.app/",
+  integrations: [ sitemap()]
+});
